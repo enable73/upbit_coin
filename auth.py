d2796c2dd56da37405f8f1a458cb54c80f0fea59
aiden <enable73@gmail.com>
2021-07-14 14:00:15 +0900

    업비트의 인증키 교체

--- auth.py
+++ auth.py
@@ -1,14 +1,11 @@
-# Python 3
-## 인증 위한 토큰 생성
-import jwt
+import jwt   # PyJWT
 import uuid
 
 payload = {
-    'access_key': '5cgQpQuihmWKVjnXctegMi1nfpeo6JCyX6Ul0EFG',
+    'access_key': '05OfEAYGLaWtUzgM4AVIDCzJw2bkrUoYJmGITiMD',
     'nonce': str(uuid.uuid4()),
 }
 
-jwt_token = jwt.encode(payload, 'iz8ELlYocxAQHsknFqUfmVU9o6s4cjGuR5GYJxHU')
+jwt_token = jwt.encode(payload, 'PlvCTMq4x9R7DgT9swYACDcxqfK9y7ZGYYoK8YvO')
 authorization_token = 'Bearer {}'.format(jwt_token)
 
-print(authorization_token)
